181cbe54600b96b966d159fe02d4a03697070472
Marta Larranaga <marta.larranaga-valsero@compagniedesalpes.fr>
2026-01-06 15:37:15 +0100

    initialise

---
+++ src/main/java/org/example/apikata/ApikataApplication.java
@@ -0,0 +1,13 @@
+package org.example.apikata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApikataApplication {
+
+    static void main() {
+        SpringApplication.run(ApikataApplication.class);
+    }
+
+}
---
+++ src/test/java/org/example/apikata/ApikataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apikata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApikataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
